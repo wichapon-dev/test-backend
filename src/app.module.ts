4a2a41350d21ecde5d89aa56486dd27f79514ab9
Wichapon Jetjongjai <wichapon-j@rmutp.ac.th>
2025-02-28 19:44:16 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -41,6 +41,8 @@ import { UserManagementModule } from './user-management/user-management.module';
       database: process.env.DATABASE_DATANAME,
       entities: [TbNew,TbRole,TbYear,TbRoom,TbComments,TbFiles,TbAdmin,TbLecturer,TbStudent],
       synchronize: true,
+    },
+      driver: require('mysql2'), // เพิ่มบรรทัดนี้
     }),
     AuthModule,
     TbRoleModule, 
